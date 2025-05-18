9de5771ff1178711a9c722cbabffd59157ec0591
mancuoj <mancuoj@gmail.com>
2025-05-18 21:11:26 +0800

    feat: movie json type, json envelope

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -6,8 +6,6 @@ import (
 	"net/http"
 	"strconv"
 
-	"maps"
-
 	"github.com/julienschmidt/httprouter"
 )
 
@@ -20,15 +18,19 @@ func (app *application) readIDParam(w http.ResponseWriter, r *http.Request) (int
 	return id, nil
 }
 
-func (app *application) writeJSON(w http.ResponseWriter, status int, data any, headers http.Header) error {
-	js, err := json.Marshal(data)
+type envelope map[string]any
+
+func (app *application) writeJSON(w http.ResponseWriter, status int, data envelope, headers http.Header) error {
+	js, err := json.MarshalIndent(data, "", "\t")
 	if err != nil {
 		return err
 	}
 
 	js = append(js, '\n')
 
-	maps.Copy(w.Header(), headers)
+	for key, value := range headers {
+		w.Header()[key] = value
+	}
 
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"greenlight.mancuoj.me/internal/data"
 )
 
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
@@ -16,5 +18,18 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 		return
 	}
 
-	fmt.Fprintf(w, "show movie %d", id)
+	movie := data.Movie{
+		ID:      id,
+		Title:   "Casablanca",
+		Year:    0,
+		Runtime: 102,
+		Genres:  []string{"drama", "romance", "war"},
+		Version: 1,
+	}
+
+	err = app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
+	if err != nil {
+		app.logger.Print(err)
+		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+	}
 }
